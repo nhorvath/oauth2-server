dcbfa911ff3121d8153c7b6abd4ce071a1ca5d03
Jacob Weber <jacob@jacobweber.com>
2016-12-12 11:11:40 -0800

    Use random_bytes instead of openssl_random_pseudo_bytes, which has some bugs. May require paragonie/random_compat library.

--- src/League/OAuth2/Server/Util/SecureKey.php
+++ src/League/OAuth2/Server/Util/SecureKey.php
@@ -26,14 +26,7 @@ class SecureKey
         // We generate twice as many bytes here because we want to ensure we have
         // enough after we base64 encode it to get the length we need because we
         // take out the "/", "+", and "=" characters.
-        $bytes = openssl_random_pseudo_bytes($len * 2, $strong);
-
-        // We want to stop execution if the key fails because, well, that is bad.
-        if ($bytes === false || $strong === false) {
-            // @codeCoverageIgnoreStart
-            throw new \Exception('Error Generating Key');
-            // @codeCoverageIgnoreEnd
-        }
+        $bytes = random_bytes($len * 2);
 
         return substr(str_replace(array('/', '+', '='), '', base64_encode($bytes)), 0, $len);
     }
